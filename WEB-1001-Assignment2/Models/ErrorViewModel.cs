c95716a04674393045c012775a536727c4959224
Zachary <zdean1995@gmail.com>
2023-04-11 16:07:00 -0400

    Add project files.

---
+++ WEB-1001-Assignment2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEB_1001_Assignment2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
