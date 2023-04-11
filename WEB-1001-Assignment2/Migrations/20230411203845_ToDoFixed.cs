8ce5e5a9d6723e19609009125fa1d99e93e890c3
Zachary <zdean1995@gmail.com>
2023-04-11 16:40:49 -0400

    Fixed an error that I was pulling my hair out over.  Turns out if you have a model that has a property with the same name as it, it causes the whole thing to not work.  Add scaffolded view for CRUD stuff for the todos.

---
+++ WEB-1001-Assignment2/Migrations/20230411203845_ToDoFixed.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WEB_1001_Assignment2.Migrations
+{
+    public partial class ToDoFixed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Todo",
+                table: "ToDos",
+                newName: "TodoText");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TodoText",
+                table: "ToDos",
+                newName: "Todo");
+        }
+    }
+}
--- WEB-1001-Assignment2/Models/ToDo.cs
+++ WEB-1001-Assignment2/Models/ToDo.cs
@@ -7,7 +7,7 @@ namespace WEB_1001_Assignment2.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        public string Todo { get; set; } = "";
+        public string TodoText { get; set; } = "";
         public Boolean Completed { get; set; } = false;
         [DataType(DataType.DateTime)]
         public DateTime? CompletetionDate { get; set; }
